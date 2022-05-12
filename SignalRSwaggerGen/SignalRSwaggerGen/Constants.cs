91b9656b094ecbf50e8e819d78ba2373b89e77ed
Mocan <dorin.mocan@mail.ru>
2022-05-12 18:25:59 +0300

    Fix DefaultHubPathTemplate

--- SignalRSwaggerGen/SignalRSwaggerGen/Constants.cs
+++ SignalRSwaggerGen/SignalRSwaggerGen/Constants.cs
@@ -6,7 +6,7 @@ namespace SignalRSwaggerGen
 	{
 		public const string HubNamePlaceholder = "[Hub]";
 		public const string MethodNamePlaceholder = "[Method]";
-		public const string DefaultHubPathTemplate = "hubs/" + HubNamePlaceholder;
+		public const string DefaultHubPathTemplate = "/hubs/" + HubNamePlaceholder;
 		public const AutoDiscover DefaultAutoDiscover = AutoDiscover.MethodsAndParams;
 		public const Operation DefaultOperation = Operation.Post;
 	}
